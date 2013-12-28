2da4f3fd5531a480ba994e4af66acb338b72bb33
Peter Jankuliak <p.jankuliak@gmail.com>
2013-12-28 15:00:43 +0100

    Add one more test to async.cpp

--- tests/async.cpp
+++ tests/async.cpp
@@ -4,12 +4,24 @@
 
 #include <masio.h>
 #include <iostream>
+#include <chrono>
 
+//------------------------------------------------------------------------------
 using namespace masio;
 using namespace std;
 namespace asio = boost::asio;
+using namespace std::chrono;
 typedef shared_ptr<State> StatePtr;
 
+//------------------------------------------------------------------------------
+system_clock::time_point now() { return system_clock::now(); }
+
+//------------------------------------------------------------------------------
+#define REQUIRE_DURATION(duration, reference) \
+  BOOST_REQUIRE_LE( abs(duration_cast<milliseconds>(duration).count() \
+                        - reference)\
+                  , 1);
+//--------------------------------------------------------------------
 BOOST_AUTO_TEST_CASE(test_async) {
   asio::io_service ios;
 
@@ -34,6 +46,63 @@ BOOST_AUTO_TEST_CASE(test_async) {
 
       BOOST_REQUIRE_EQUAL(rs[0].value(), 11);
       BOOST_REQUIRE_EQUAL(rs[1].value(), 22);
+
+      executed = true;
+      });
+
+  int poll_count = 0;
+
+  while(ios.run_one()) {
+    ++poll_count;
+  }
+
+  BOOST_REQUIRE(executed);
+  BOOST_REQUIRE_EQUAL(poll_count, 2);
+}
+
+//--------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(test_async_sleep) {
+  asio::io_service ios;
+
+  StatePtr state = make_shared<State>();
+
+  typedef system_clock::time_point Time;
+
+  unsigned int duration0 = 123;
+  unsigned int duration1 = 234;
+
+  Cont<Time>::Ptr p0 = sleep<Time>(ios, duration0, []() {
+      return success<Time>(now());
+      });
+
+  Cont<Time>::Ptr p1 = sleep<Time>(ios, duration1, []() {
+      return success<Time>(now());
+      });
+
+  Async<Time>::Ptr p = async<Time>(p0, p1);
+
+  bool executed = false;
+
+  auto start = now();
+
+  p->run(state, [&executed, start, duration0, duration1]
+                (Error<std::vector<Error<Time>>> ers) {
+      BOOST_REQUIRE(!ers.is_error());
+
+      const vector<Error<Time>>& rs = ers.value();
+
+      BOOST_REQUIRE_EQUAL(rs.size(), 2);
+
+      BOOST_REQUIRE(rs[0].is_value());
+      BOOST_REQUIRE(rs[1].is_value());
+
+      auto end = now();
+      auto t0  = rs[0].value();
+      auto t1  = rs[1].value();
+
+      REQUIRE_DURATION(t0 - start, duration0);
+      REQUIRE_DURATION(t1 - start, duration1);
+      REQUIRE_DURATION(end - start, max(duration0, duration1));
       
       executed = true;
       });
